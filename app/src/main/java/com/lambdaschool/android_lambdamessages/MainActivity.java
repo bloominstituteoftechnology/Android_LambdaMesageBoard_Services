f034b9dfec3106de1b2f07432966e0b1c96b829a
heiligbasil <basilief@yahoo.com>
2019-04-04 12:27:42 -0700

    Added SubscriptionMonitorService, override methods, and added logging

--- app/src/main/java/com/lambdaschool/android_lambdamessages/MainActivity.java
+++ app/src/main/java/com/lambdaschool/android_lambdamessages/MainActivity.java
@@ -3,6 +3,7 @@ package com.lambdaschool.android_lambdamessages;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
---
+++ app/src/main/java/com/lambdaschool/android_lambdamessages/SubscriptionMonitorService.java
@@ -0,0 +1,39 @@
+package com.lambdaschool.android_lambdamessages;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+
+public class SubscriptionMonitorService extends Service {
+    private static final String TAG = "SubscriptionMonSvc in ";
+
+    public SubscriptionMonitorService() {
+        Log.i(TAG, "constructor " + Thread.currentThread().getStackTrace()[2].getMethodName());
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        Log.i(TAG, Thread.currentThread().getStackTrace()[2].getMethodName());
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.i(TAG, Thread.currentThread().getStackTrace()[2].getMethodName());
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.i(TAG, Thread.currentThread().getStackTrace()[2].getMethodName());
+
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG, Thread.currentThread().getStackTrace()[2].getMethodName());
+    }
+}
