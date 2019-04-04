e1b72450e13fe89de87d3c8567a3e7b598c2c311
VivekV95 <vivekv210@gmail.com>
2019-04-04 16:05:51 -0500

    App sends notification when message board is updated

--- app/src/main/java/com/vivekvishwanath/android_lambdamesageboard_services/SubscriptionMonitorService.java
+++ app/src/main/java/com/vivekvishwanath/android_lambdamesageboard_services/SubscriptionMonitorService.java
@@ -1,11 +1,26 @@
 package com.vivekvishwanath.android_lambdamesageboard_services;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.IBinder;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
+import java.util.ArrayList;
+
 public class SubscriptionMonitorService extends Service {
+
+    private static long lastCheckTime;
+    String subscription;
+    private Context context;
+    private static final int NOTIFICATION_ID = 11;
+    private static final int CHECK_PERIOD = 5000;
+
     public SubscriptionMonitorService() {
     }
 
@@ -18,14 +33,59 @@ public class SubscriptionMonitorService extends Service {
     @Override
     public void onCreate() {
         Log.i("Service", "Entered onCreate");
+        lastCheckTime = System.currentTimeMillis() / 1000;
+        subscription = "";
+        context = this;
         super.onCreate();
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         Log.i("Service", "Entered onStartCommand");
-        String boardIdentifier = intent.getStringExtra("add_subscription");
-        stopSelf();
+        subscription = intent.getStringExtra("add_subscription");
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                boolean flag = false;
+                while (subscription != "") {
+                    ArrayList<MessageBoard> messageBoards = MessageBoardDao.getMessageBoards();
+                    for (int i = 0; i < messageBoards.size(); i++) {
+                        String identifier = messageBoards.get(i).getIdentifier();
+                        if (identifier.equals(subscription)) {
+                            int numMessages = messageBoards.get(i).getMessages().size();
+                            double lastMessageTime = messageBoards.get(i).getMessages().get(numMessages - 1).getTimestamp();
+                            if (lastMessageTime > lastCheckTime) {
+                                flag = true;
+                            }
+                        }
+                    }
+                    lastCheckTime = System.currentTimeMillis() / 1000;
+                    if (flag) {
+                        NotificationManager manager = (NotificationManager) context.getSystemService(context.NOTIFICATION_SERVICE);
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                            CharSequence name = getPackageName();
+                            String description = "MessageBoard Subscription Channel";
+                            int importance = NotificationManager.IMPORTANCE_HIGH;
+                            NotificationChannel channel = new NotificationChannel(getPackageName(), description, importance);
+
+                            manager.createNotificationChannel(channel);
+                        }
+                        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, getPackageName())
+                                .setContentTitle("New Message!")
+                                .setContentText("New message in your message board")
+                                .setSmallIcon(R.drawable.ic_launcher_foreground)
+                                .setAutoCancel(true);
+                        manager.notify(NOTIFICATION_ID, builder.build());
+                    }
+                    try {
+                        Thread.sleep(5000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                stopSelf();
+            }
+        }).start();
         return super.onStartCommand(intent, flags, startId);
     }
 
